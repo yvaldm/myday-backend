b00e50b8e717b87091d9105dd0d2a2a6afcc3736
Valery Yakovlev <vyakovlev@altruist.com>
2019-09-14 09:30:25 +0300

    dynamic tables generation from embedded pg

--- src/main/java/com/yvaldm/myday/mydaybackend/dao/impl/ActionDaoImpl.java
+++ src/main/java/com/yvaldm/myday/mydaybackend/dao/impl/ActionDaoImpl.java
@@ -1,22 +1,22 @@
 package com.yvaldm.myday.mydaybackend.dao.impl;
 
-import com.yvaldm.myday.mydaybackend.dao.ActionDao;
-import com.yvaldm.myday.mydaybackend.dao.generated.Tables;
-import com.yvaldm.myday.mydaybackend.entity.Action;
-import org.jooq.DSLContext;
+import static com.yvaldm.myday.server.jooq.Tables.ACTION;
 
+import com.yvaldm.myday.mydaybackend.dao.ActionDao;
+import com.yvaldm.myday.mydaybackend.entity.Action;
 import java.util.List;
+import org.jooq.DSLContext;
 
 public class ActionDaoImpl implements ActionDao {
 
-    private final DSLContext dslContext;
+  private final DSLContext dslContext;
 
-    public ActionDaoImpl(DSLContext dslContext) {
-        this.dslContext = dslContext;
-    }
+  public ActionDaoImpl(DSLContext dslContext) {
+    this.dslContext = dslContext;
+  }
 
-    @Override
-    public List<Action> findActions() {
-        return dslContext.selectFrom(Tables.ACTION).fetchInto(Action.class);
-    }
+  @Override
+  public List<Action> findActions() {
+    return dslContext.selectFrom(ACTION).fetchInto(Action.class);
+  }
 }
--- src/main/java/com/yvaldm/myday/mydaybackend/dao/impl/ActionPerformedDaoImpl.java
+++ src/main/java/com/yvaldm/myday/mydaybackend/dao/impl/ActionPerformedDaoImpl.java
@@ -1,56 +1,55 @@
 package com.yvaldm.myday.mydaybackend.dao.impl;
 
+import static com.yvaldm.myday.server.jooq.Tables.PERFORMED_ACTION;
+
 import com.yvaldm.myday.mydaybackend.dao.ActionPerformedDao;
 import com.yvaldm.myday.mydaybackend.entity.ActionPerformed;
+import java.sql.Timestamp;
+import java.util.List;
 import org.jooq.DSLContext;
 import org.jooq.DatePart;
 import org.jooq.impl.DSL;
 
-import java.sql.Timestamp;
-import java.util.List;
-
-import static com.yvaldm.myday.mydaybackend.dao.generated.Tables.PERFORMED_ACTION;
-
 public class ActionPerformedDaoImpl implements ActionPerformedDao {
 
-    private final DSLContext dslContext;
+  private final DSLContext dslContext;
 
-    public ActionPerformedDaoImpl(DSLContext dslContext) {
-        this.dslContext = dslContext;
-    }
+  public ActionPerformedDaoImpl(DSLContext dslContext) {
+    this.dslContext = dslContext;
+  }
 
-    @Override
-    public void insertActionTrack(String userId, long actionId) {
+  @Override
+  public void insertActionTrack(String userId, long actionId) {
 
-        dslContext.insertInto(PERFORMED_ACTION)
-            .set(PERFORMED_ACTION.ACTION_ID, actionId)
-            .set(PERFORMED_ACTION.USER_ID, userId)
-            .execute();
-    }
+    dslContext.insertInto(PERFORMED_ACTION)
+        .set(PERFORMED_ACTION.ACTION_ID, actionId)
+        .set(PERFORMED_ACTION.USER_ID, userId)
+        .execute();
+  }
 
-    @Override
-    public List<ActionPerformed> findTrackedActionsOfToday(String userId) {
+  @Override
+  public List<ActionPerformed> findTrackedActionsOfToday(String userId) {
 
-        return dslContext.selectFrom(PERFORMED_ACTION)
-            .where(PERFORMED_ACTION.USER_ID.eq(userId))
-            .and(PERFORMED_ACTION.CTS.ge(DSL.currentDate().cast(Timestamp.class)))
-            .fetchInto(ActionPerformed.class);
-    }
+    return dslContext.selectFrom(PERFORMED_ACTION)
+        .where(PERFORMED_ACTION.USER_ID.eq(userId))
+        .and(PERFORMED_ACTION.CTS.ge(DSL.currentDate().cast(Timestamp.class)))
+        .fetchInto(ActionPerformed.class);
+  }
 
-    @Override
-    public List<ActionPerformed> findTrackedActions(String userId) {
+  @Override
+  public List<ActionPerformed> findTrackedActions(String userId) {
 
-        return dslContext.selectFrom(PERFORMED_ACTION)
-            .where(PERFORMED_ACTION.USER_ID.eq(userId))
-            .fetchInto(ActionPerformed.class);
-    }
+    return dslContext.selectFrom(PERFORMED_ACTION)
+        .where(PERFORMED_ACTION.USER_ID.eq(userId))
+        .fetchInto(ActionPerformed.class);
+  }
 
-    @Override
-    public List<ActionPerformed> findTrackedActionsByMonth(String userId, int year, int month) {
+  @Override
+  public List<ActionPerformed> findTrackedActionsByMonth(String userId, int year, int month) {
 
-        return dslContext.selectFrom(PERFORMED_ACTION)
-            .where(DSL.extract(PERFORMED_ACTION.CTS, DatePart.MONTH).eq(month))
-            .and(DSL.extract(PERFORMED_ACTION.CTS, DatePart.YEAR).eq(year))
-            .fetchInto(ActionPerformed.class);
-    }
+    return dslContext.selectFrom(PERFORMED_ACTION)
+        .where(DSL.extract(PERFORMED_ACTION.CTS, DatePart.MONTH).eq(month))
+        .and(DSL.extract(PERFORMED_ACTION.CTS, DatePart.YEAR).eq(year))
+        .fetchInto(ActionPerformed.class);
+  }
 }
